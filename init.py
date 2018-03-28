c9ddd0774ece19b9e2165b263f296707a89231bc
Avery Wagar <ajmw.subs@gmail.com>
2018-03-28 10:56:22 -0700

    Restructure toolkit

--- init.py
+++ init.py
@@ -1,6 +1,15 @@
-COSLAW = __import__('coslaw')
-BRAD = __import__('brad')
-GEO = __import__('geo')
+import sys
+sys.path.insert(0, './tools/')
+
+import coslaw
+import geo
+import brad
+
+
+
+# coslaw = __import__('./tools/coslaw')
+# brad = __import__('./tools/brad')
+# geo = __import__('./tools/geo')
 # psqr    = __import__('psqr')
 
 print("Welcome to the Geometry Toolkit\n==============================\nCreated by: Avery Wagar & Erik Anderson")
@@ -10,12 +19,11 @@ while not DONE:
     choice = input("What tool would you like to use? (radical, trig, geo) or type quit to exit > ")
 
     if choice == "trig":
-        COSLAW.solve(input("What equation would you like to solve? (sin, cos, rsin, rcos, dis) or type cancel to cancel > "))
+        coslaw.solve(input("What equation would you like to solve? (sin, cos, rsin, rcos, dis) or type cancel to cancel > "))
     elif choice == "radical":
-        BRAD.solve(float(input("Please enter your number: ")))
+        brad.solve(float(input("Please enter your number: ")))
     elif choice == "geo":
-        GEO.solve(input("Which equation would you like to solve? (sum, each, ext_sum,
-                        ext_each) > "))
+        geo.solve(input("Which equation would you like to solve? (sum, each, ext_sum, ext_each) > "))
     elif choice == "clear":
         import os
         clear = lambda: os.system('clear')
